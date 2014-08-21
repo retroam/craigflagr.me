f8caa18a0182e5aa83386061c658c100ac404f7d
Your Name <retroam@yahoo.com>
2014-08-21 08:54:50 -0400

    Change craigslist_download_disk

--- craigslist_download_disk.py
+++ craigslist_download_disk.py
@@ -5,8 +5,6 @@ from os import listdir
 API_KEY = '082906284971364c1cb52da644536e37'
 client = threetaps.Threetaps(API_KEY)
 
-ANCHOR = 1281205933
-
 RETVALS = ['id', 'account_id', 'source', 'category', 'category_group', 'location',
            'external_id', 'external_url', 'heading', 'body', 'timestamp', 'timestamp_deleted',
            'expires', 'language', 'price', 'currency', 'images', 'annotations', 'status',
@@ -17,26 +15,20 @@ FILE_PATH = '/home/ubuntu/data_dump'
 number_files = 10000
 
 files = listdir(FILE_PATH)
+page = 0
 
+response = client.search.search(params={'source': 'CRAIG',
+                                            'retvals': ','.join(RETVALS),
+                                            'sort': 'timestamp',
+                                            'status': 'for_rent',
+                                            'rpp': 1,
+                                            'page': page})
 
-def file_num(a):
-    return int(a[5:-5])
-files_trunc = files[1:]
-
-last_file = sorted(files_trunc, key=file_num)[-1]
-print last_file
-with open(FILE_PATH + '/' + last_file) as f:
-        my_dict = json.load(f)
-        next_page = my_dict['next_page']
-
-page = next_page
-print next_page
-
+ANCHOR = response['anchor']
 for i in range(number_files):
     print str(page)
-
     response = client.search.search(params={'source': 'CRAIG',
-                                            'retvals':','.join(RETVALS),
+                                            'retvals': ','.join(RETVALS),
                                             'sort': 'timestamp',
                                             'status': 'for_rent',
                                             'anchor': str(ANCHOR),
